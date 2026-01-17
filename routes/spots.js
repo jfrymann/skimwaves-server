b3361d618f6f91773fdd7a260ba3219120e3e7e4
Justin Frymann <jfrymann@gmail.com>
2026-01-17 03:28:00 -0800

    editing geolocation

--- routes/spots.js
+++ routes/spots.js
@@ -16,14 +16,13 @@ router.get("/spot-info/:slug/:_id", async (req, res) => {
   let collection = await db.collection("spots");
   let query = { _id: new ObjectId(req.params._id) };
   let result = await collection.findOne(query);
-  console.log(result)
 
   if (!result) res.send("Not found").status(404);
   else res.send(result).status(200);
 });
 
 router.get("/spot-info/edit/:slug/:_id", async (req, res) => {
-  console.log('updating ' + req.params.slug)
+  console.log('getting ' + req.params.slug)
 
   let collection = await db.collection("spots");
   let query = { _id: new ObjectId(req.params._id) };
@@ -31,17 +30,16 @@ router.get("/spot-info/edit/:slug/:_id", async (req, res) => {
 });
 
 router.patch("/spot-info/edit/:slug/:_id", async (req, res) => {
-  console.log('updating ' + req.params.slug)
-
   let collection = await db.collection("spots");
   collection.updateOne(
     { _id: new ObjectId(req.params._id) },
     {
       $set: {
+        spotName: req.body.spotName,
         description: req.body.description,
         bestTide: req.body.bestTide,
         bestHeight: req.body.bestHeight,
-        slug: req.body.slug,
+        "location.coordinates": [req.body.location.coordinates[0], req.body.location.coordinates[1]],
       }
     }
   )
